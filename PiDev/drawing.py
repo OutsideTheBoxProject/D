2b24330ab9bae907a08a09d9bee2207c3bb447af
kattaspiel <katta@igw.tuwien.ac.at>
2015-05-27 13:10:01 +0000

    better drawing

--- PiDev/drawing.py
+++ PiDev/drawing.py
@@ -4,35 +4,55 @@
 import pygame, random
 from pygame.locals import *
 
+# random colour picker
+def pick_colour():
+	r = random.randint(0, 255)
+	g = random.randint(0, 255)
+	b = random.randint(0, 255)
+	return (r, g, b)
+	
 # instructions
 print "ESC Key to exit, click to change colour."
 
 # initialize screen
 pygame.init()
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((1024, 592), pygame.FULLSCREEN)
 screen.fill((255, 255, 255))
 pygame.display.flip()
 
 # initilize with some colour
-r = 150
-g = 200
-b = 50
+(r, g, b) = pick_colour()
+
+# control variable for mouse
+mouseDown = False
+
+# control variable for line points
+prevx = prevy = mousex = mousey = -1
+
 
 # Pygame Loop
 while True:
 	for event in pygame.event.get():
 		
 		#drawing
-		if event.type == MOUSEMOTION:
-			mousex, mousey = event.pos
-			pygame.draw.circle(screen, (r, g, b), (mousex, mousey), 5, 0)
-			pygame.display.flip()
+		if event.type == MOUSEMOTION and mouseDown:
+			if prevx == -1:
+				prevx, prevy = event.pos
+			else:
+				mousex, mousey = event.pos
+				pygame.draw.line(screen, (r, g, b), (prevx, prevy), (mousex, mousey), 1)
+				(prevx, prevy) = (mousex, mousey)
+				pygame.display.flip()
 		
 		#change of colour
 		if event.type == MOUSEBUTTONDOWN:
-			r = random.randint(0, 255)
-			g = random.randint(0, 255)
-			b = random.randint(0, 255)
+			(r, g, b) = pick_colour()
+			mouseDown = True
+			
+		#toggle mouse button
+		if event.type == MOUSEBUTTONUP:
+			prevx = prevy = -1
+			mouseDown = False
 			
 		#exit
 		if event.type == pygame.KEYDOWN:
